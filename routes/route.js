fcc869ff5be375f5f3bdd1699b19f0d3d69f2765
Honey Andharia <honey.andharia@esparkbizmail.com>
2024-04-10 13:04:40 +0000

    I2

--- routes/route.js
+++ routes/route.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { userLogin, getLogin } = require('../controller/login/login');
-const { getHome } = require('../controller/home/homeController')
+const { getHome } = require('../controller/home/homeController');
 const { auth } = require('../middleware/auth');
 const { getForgot, forgotpass } = require('../controller/login/forgot');
 const { listManagers } = require('../controller/manager/manager');
-const getsales = require('../controller/sales_module/sales_data');
-const insert_order = require('../controller/sales_module/insert_order');
+
 const { stores } = require('../controller/stores/store.js');
 const passport = require('passport');
 router.use(passport.initialize());
 auth(passport);
 
-
-const { insertCustomer, updateCustomer, getCustomers, deleteCustomer, filterCustomer } = require('../controller/manageCustomers/manageCustomers.js');
-
+const {
+	insertCustomer,
+	updateCustomer,
+	getCustomers,
+	deleteCustomer,
+	filterCustomer,
+} = require('../controller/manageCustomers/manageCustomers.js');
 
 //---------Manage Customers
 router.get('/home', passport.authenticate('jwt', { session: false }), getHome);
@@ -25,11 +28,10 @@ router.get('/manager', listManagers);
 
 router.post('/forgot', forgotpass);
 
-
 //sales Module
 
-const getsales = require("../controller/salesModule/salesData");
-router.get("/salesorder", getsales);
+const getorders = require('../controller/salesModule/salesData');
+router.get('/salesorder', getorders);
 
 const insertOrder = require('../controller/salesModule/addOrder');
 router.post('/insertSalesOrder', insertOrder);
@@ -40,8 +42,8 @@ router.post('/insertProduct', insertProduct);
 const getProducts = require('../controller/salesModule/getProducts');
 router.get('/getProducts', getProducts);
 
-const getCustomers = require('../controller/salesModule/getCustomers');
-router.get('/getCustomers', getCustomers);
+const getCustomersSales = require('../controller/salesModule/getCustomers');
+router.get('/getCustomers', getCustomersSales);
 
 const updateOrder = require('../controller/salesModule/updateOrders');
 router.post('/updateOrder', updateOrder);
