26c0ef4737921a0576a7a44613d47f7872e3e1b7
aryamankarde2024 <aryaman.karde.2024@gmail.com>
2024-04-10 17:08:33 +0530

    Resolved current merge conflicts of main branch.

--- controller/login/login.js
+++ controller/login/login.js
@@ -1,68 +1,66 @@
-const {userLoginService,logsService,logUnsuccessService} = require("../../service/login/login")
-const md5 = require("md5");
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+const {
+	userLoginService,
+	logsService,
+	logUnsuccessService,
+} = require('../../service/login/login');
+const md5 = require('md5');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const { SECRET_KEY } = process.env;
-<<<<<<< HEAD:controller/loginController.js
-=======
 
-const getLogin=async(req,res)=>[
-  res.render('login/login')
-]
+const getLogin = async (req, res) => [res.render('login/login')];
 
->>>>>>> 6c03b51345e006ec04055d79d4e604df6eeabcb5:controller/login/login.js
-const userLogin=async(req,res)=>{
-  try {
-    const user=await userLoginService(req.body);
-    console.log(user);
-    if(user.length>0 && user[0].status==1){
-      const password=md5(user[0].salt+req.body.password);
-      if(user[0].role_id==4){
-          if(password==user[0].password){
-            const email=req.body.email; 
-            const token=jwt.sign({email:email},SECRET_KEY,{expiresIn:'2h'});
-            const id=user[0].id;
-            const logs=await logsService(id);
-            return res
-            .cookie("token",token,{
-              httpOnly:true
-            })
-            .redirect(`/home`);
-          
-          }else{
-            console.log(user,"needddd");
-            const id=user[0].id
-            const log=await logUnsuccessService(id);
-            const error="invalid password"
-            res.render("login/login",{error})
-          }
-      }else if(user[0].role_id==5){
-        if(password==user[0].password){
-          const email=req.body.email;
-          const token=jwt.sign({email:email},SECRET_KEY,{expiresIn:'1h'});
-          return res
-          .cookie("token",token,{
-            httpOnly:true
-          })
-          .redirect(`/home`)
-        }else{
-          const log=await logUnsuccessService(id);
-          const error="invalid password"
-          res.render("login/login",{error})
-        }
-      }
-    }else if(user.length===0){
-      const error="user not exist"
-      res.render("login/login",{error})
-    }
-  } catch (error) {
-    console.error("error", error);
-    res.status(500).json({ message: "can`t fetch user controller" })
-  }
-}
-<<<<<<< HEAD:controller/loginController.js
-module.exports={userLogin}
-=======
-module.exports={userLogin,getLogin,logsService,logUnsuccessService}
->>>>>>> 6c03b51345e006ec04055d79d4e604df6eeabcb5:controller/login/login.js
+const userLogin = async (req, res) => {
+	try {
+		const user = await userLoginService(req.body);
+		console.log(user);
+		if (user.length > 0 && user[0].status == 1) {
+			const password = md5(user[0].salt + req.body.password);
+			if (user[0].role_id == 4) {
+				if (password == user[0].password) {
+					const email = req.body.email;
+					const token = jwt.sign({ email: email }, SECRET_KEY, {
+						expiresIn: '2h',
+					});
+					const id = user[0].id;
+					const logs = await logsService(id);
+					return res
+						.cookie('token', token, {
+							httpOnly: true,
+						})
+						.redirect(`/home`);
+				} else {
+					console.log(user, 'needddd');
+					const id = user[0].id;
+					const log = await logUnsuccessService(id);
+					const error = 'invalid password';
+					res.render('login/login', { error });
+				}
+			} else if (user[0].role_id == 5) {
+				if (password == user[0].password) {
+					const email = req.body.email;
+					const token = jwt.sign({ email: email }, SECRET_KEY, {
+						expiresIn: '1h',
+					});
+					return res
+						.cookie('token', token, {
+							httpOnly: true,
+						})
+						.redirect(`/home`);
+				} else {
+					const log = await logUnsuccessService(id);
+					const error = 'invalid password';
+					res.render('login/login', { error });
+				}
+			}
+		} else if (user.length === 0) {
+			const error = 'user not exist';
+			res.render('login/login', { error });
+		}
+	} catch (error) {
+		console.error('error', error);
+		res.status(500).json({ message: 'can`t fetch user controller' });
+	}
+};
+module.exports = { userLogin, getLogin, logsService, logUnsuccessService };
--- routes/route.js
+++ routes/route.js
@@ -1,51 +1,26 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const {userLogin}=require('../controller/loginController');
-=======
-const {userLogin,getLogin}=require('../controller/login/login');
-const {getHome}=require('../controller/home/homeController')
->>>>>>> 6c03b51345e006ec04055d79d4e604df6eeabcb5
+const { userLogin, getLogin } = require('../controller/login/login');
+const { getHome } = require('../controller/home/homeController');
 const { auth } = require('../middleware/auth');
-const {getForgot,forgotpass}=require('../controller/login/forgot')
-const getsales = require("../controller/sales_module/sales_data");
+const { getForgot, forgotpass } = require('../controller/login/forgot');
+const getsales = require('../controller/sales_module/sales_data');
 const insert_order = require('../controller/sales_module/insert_order');
 const passport = require('passport');
 router.use(passport.initialize());
 auth(passport);
 
-<<<<<<< HEAD
-router.get('/login',(req,res)=>{
-  res.render('login');
-})
-router.post('/login',userLogin);
+router.get('/', getLogin);
+router.post('/', userLogin);
 
+router.get('/home', passport.authenticate('jwt', { session: false }), getHome);
 
-router.get('/home',passport.authenticate("jwt",{session:false}),(req,res)=>{
-  res.render('home');
-})
+router.get('/forgot', getForgot);
 
-router.get('/forgot',(req,res)=>{
-  res.render('forgot');
-})
-router.get("/store",(req, res) => {
-  res.render("stores/store", { message: "Not Found" });
-});
-const getsales = require("../controller/sales_module/sales_data");
-const insert_order = require('../controller/sales_module/insert_order');
-=======
-router.get('/',getLogin);
-router.post('/',userLogin);
+router.post('/forgot', forgotpass);
 
-router.get('/home',passport.authenticate("jwt",{session:false}),getHome);
-
-router.get('/forgot',getForgot);
-
-router.post('/forgot',forgotpass);
-
->>>>>>> 6c03b51345e006ec04055d79d4e604df6eeabcb5
-router.get("/salesorder", getsales);
-router.post('/insert',insert_order);
+router.get('/salesorder', getsales);
+router.post('/insert', insert_order);
 
 module.exports = router;
