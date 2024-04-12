e962bb28262af3ff234116b530ed53b2c7f35cfa
Dhyeypatel24 <dhyey.patelinfo2024@gmail.com>
2024-04-12 10:53:39 +0530

    pull maim and change in navigation

--- service/stores/store.js
+++ service/stores/store.js
@@ -1,19 +1,23 @@
-const connection = require('../../config/connection.js');
-const logger = require('../../logs.js');
+const connection = require("../../config/connection.js");
+const logger = require("../../logs.js");
 
 async function insertStoreQuery(body) {
   try {
-    const insertStore = `INSERT INTO storage_space_master (name,storage_type,location_id) VALUES(?,?,?);`
-    const [result] = await connection.execute(insertStore, [body.storageName,body.storeType,body.location]);
+    const insertStore = `INSERT INTO storage_space_master (name,storage_type,location_id) VALUES(?,?,?);`;
+    const [result] = await connection.execute(insertStore, [
+      body.storageName,
+      body.storeType,
+      body.location,
+    ]);
     console.log(result);
-} catch (error) {
+  } catch (error) {
     logger.logError("Insert Store: " + error);
   }
 }
 
 async function getStoreQuery() {
   try {
-    const getStores = `SELECT storage_space_master.name , option_master.value FROM storage_space_master inner join option_master on storage_space_master.storage_type = option_master.id;`
+    const getStores = `SELECT storage_space_master.name , option_master.value FROM storage_space_master inner join option_master on storage_space_master.storage_type = option_master.id;`;
 
     const [result] = await connection.execute(getStores);
     return result; //return array
@@ -25,12 +29,15 @@ async function getStoreQuery() {
 
 async function updateStoreQuery(body) {
   try {
-
     if (customerArray.length === 0) {
       return false;
     } else {
-      const updateStore = `UPDATE storage_space_master SET name=? storage_type = ?, location_id=? WHERE id=?;`
-      const [result] = connection.execute(updateStore, [body.storagename,body.storagetype, body.location]);
+      const updateStore = `UPDATE storage_space_master SET name=? storage_type = ?, location_id=? WHERE id=?;`;
+      const [result] = connection.execute(updateStore, [
+        body.storagename,
+        body.storagetype,
+        body.location,
+      ]);
       return true;
     }
   } catch (error) {
@@ -38,4 +45,4 @@ async function updateStoreQuery(body) {
   }
 }
 
-module.exports = { insertStoreQuery,getStoreQuery,updateStoreQuery };
+module.exports = { insertStoreQuery, getStoreQuery, updateStoreQuery };
