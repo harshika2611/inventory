87eeba3219b3de65fbfb510b29aa1c484585e297
Keyur Purohit <keyur.purohit@esparkbizmail.com>
2024-04-12 13:33:28 +0000

    Route and Template for View profile and Edit profile

---
+++ controller/profile/profile.js
@@ -0,0 +1,34 @@
+// const profileViewService = require('../../service/profile/viewProfile');
+
+const logger = require("../../logs");
+
+async function viewProfile(req, res) {
+	// try {
+	// 	const profileDetails = await profileViewService(req.body);
+	// 	// return res.status(200).json({ data: profileDetails });
+	// 	return res.status(200).render('./profile/view', { data: profileDetails });
+	// } catch (error) {
+	// 	return res.status(404).json({ message: "Can't get profile details" });
+	// }
+	try{
+		res.render('./profile/view');
+	}
+	catch(err){
+		logger.logError(err);
+	}
+	
+}
+
+async function editProfile(req,res){
+	try{
+		res.render('./profile/edit');
+	}
+	catch(err){
+		logger.logError(err);
+	}
+}
+
+module.exports = {
+	viewProfile,
+	editProfile,
+};
