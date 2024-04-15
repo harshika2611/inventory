87ebac0027fc8034f738460d554445097a1e4dcf
Harshika Rabadiya <harshika.rabadiya@esparkbizmail.com>
2024-04-15 09:19:01 +0000

    Manager registration and Ui

--- service/manageCustomers/manageCustomers.js
+++ service/manageCustomers/manageCustomers.js
@@ -3,69 +3,94 @@ const logger = require('../../logs.js');
 const { getCityStateId } = require('../commonFunctions/commonFunctions.js');
 
 async function insertCustomerQuery(body) {
-  // return new Promise((resolve, reject) => {
+	// return new Promise((resolve, reject) => {
 
-  // });
-  try {
-    const insertCustomer = `INSERT INTO customer_maste(firstname,lastname,email,phonenumber,address,zipcode,city_id,state_id) VALUES(?,?,?,?,?,?,?,?);`
+	// });
+	try {
+		const insertCustomer = `INSERT INTO customer_maste(firstname,lastname,email,phonenumber,address,zipcode,city_id,state_id) VALUES(?,?,?,?,?,?,?,?);`;
 
-    const cityStateId = await getCityStateId(body.state, body.city);  //array contain object
+		const cityStateId = await getCityStateId(body.state, body.city); //array contain object
 
-    const [result] = await connection.execute(insertCustomer, [body.firstname, body.lastname, body.email, body.phonenumber, body.address, body.zipcode, cityStateId[0].city_id, cityStateId[0].state_id]);
-  } catch (error) {
-    logger.logError("Insert Customer: " + error);
-    throw error;
-  }
+		const [result] = await connection.execute(insertCustomer, [
+			body.firstname,
+			body.lastname,
+			body.email,
+			body.phonenumber,
+			body.address,
+			body.zipcode,
+			cityStateId[0].city_id,
+			cityStateId[0].state_id,
+		]);
+	} catch (error) {
+		logger.logError('Insert Customer: ' + error);
+		throw error;
+	}
 }
 
 async function getCustomersQuery() {
-  try {
-    const getCustomers = `SELECT * FROM customer_master;`
+	try {
+		const getCustomers = `SELECT * FROM customer_master;`;
 
-    const [result] = await connection.execute(getCustomers);
-    return result; //return array
-  } catch (error) {
-    logger.logError("Get Customers: " + error);
-    return [];
-  }
+		const [result] = await connection.execute(getCustomers);
+		return result; //return array
+	} catch (error) {
+		logger.logError('Get Customers: ' + error);
+		return [];
+	}
 }
 
 async function checkCustomerExistQuery(customerId) {
-  try {
-    const checkCustomer = `SELECT * as customerId FROM customer_master WHERE id=?;`
+	try {
+		const checkCustomer = `SELECT * as customerId FROM customer_master WHERE id=?;`;
 
-    const [result] = await connection.execute(checkCustomer, [customerId]);
-    return result;
-  } catch (error) {
-    logger.logError("Check customer: " + error);
-  }
+		const [result] = await connection.execute(checkCustomer, [customerId]);
+		return result;
+	} catch (error) {
+		logger.logError('Check customer: ' + error);
+	}
 }
 
 async function updateCustomerQuery(customerId, body) {
-  try {
-    const customerArray = checkCustomerExistQuery(customerId);
+	try {
+		const customerArray = checkCustomerExistQuery(customerId);
 
-    if (customerArray.length === 0) {
-      return false;
-    } else {
-      const updateCustomer = `UPDATE customer_master SET fname = ?, lname=?, email=?,phonenumber=?,email=?,address=?,zipcode=?,city=?,state=? WHERE id=?;`
+		if (customerArray.length === 0) {
+			return false;
+		} else {
+			const updateCustomer = `UPDATE customer_master SET firstname = ?, lastname=?, email=?,phonenumber=?,email=?,address=?,zipcode=?,city=?,state=? WHERE id=?;`;
 
-      const [result] = connection.execute(updateCustomer, [body.fname, body.lname, body.email, body.phonenumber, body.email, body.address, body.zipcode, body.cityId, body.stateId, customerId]);
-      return true;
-    }
-  } catch (error) {
-    logger.logError("Update Customer: " + error);
-  }
+			const [result] = connection.execute(updateCustomer, [
+				body.firstname,
+				body.lastname,
+				body.email,
+				body.phonenumber,
+				body.email,
+				body.address,
+				body.zipcode,
+				body.cityId,
+				body.stateId,
+				customerId,
+			]);
+			return true;
+		}
+	} catch (error) {
+		logger.logError('Update Customer: ' + error);
+	}
 }
 
 async function deleteCustomerQuery(customerId) {
-  try {
-    const deleteCustomer = `DELETE customer_master WHERE id=?`;
+	try {
+		const deleteCustomer = `DELETE customer_master WHERE id=?`;
 
-    const [result] = connection.execute(deleteCustomer, [customerId]);
-  } catch (error) {
-    logger.logError("Delete Customer: " + error);
-  }
+		const [result] = connection.execute(deleteCustomer, [customerId]);
+	} catch (error) {
+		logger.logError('Delete Customer: ' + error);
+	}
 }
 
-module.exports = { insertCustomerQuery, getCustomersQuery, updateCustomerQuery, deleteCustomerQuery };
+module.exports = {
+	insertCustomerQuery,
+	getCustomersQuery,
+	updateCustomerQuery,
+	deleteCustomerQuery,
+};
--- service/profile/profile.js
+++ service/profile/profile.js
@@ -2,7 +2,7 @@ const connection = require('../../config/connection');
 
 async function viewProfileQuery(body) {
 	try {
-		const getCustomers = `select fname,lname,dob,email,option_master.value from users inner join option_master on users.role_id = option_master.id where email = ?`;
+		const getCustomers = `select firstname,lastname,dob,email,option_master.value from users inner join option_master on users.role_id = option_master.id where email = ?`;
 
 		const [result] = await connection.execute(getCustomers, [body.email]);
 		return result; //return array
