d7947c29d05432a2fb5630d78fdc5fb812a65d45
Aryaman Karde <aryaman.karde@esparkbizmail.com>
2024-04-17 14:34:15 +0000

    Completed Purchase Order Module (Except Deletion)

--- controller/purchase/index.js
+++ controller/purchase/index.js
@@ -4,6 +4,9 @@ const {
 	getAllProducts,
 	getAllWarehouses,
 	addProductInPurchaseOrder,
+	fetchPurchaseOrder,
+	updatePurchaseOrder,
+	updateProductInPurchaseOrder,
 } = require('../../service/purchase');
 const { getCombos } = require('../../service/helper');
 
@@ -39,6 +42,36 @@ async function fetchWarehouses(_, res) {
 	}
 }
 
+async function fetchOrderDetails(req, res) {
+	try {
+		const finalResponse = {};
+		const response = await fetchPurchaseOrder({ id: req.params.id });
+
+		if (response.length > 0) {
+			finalResponse.purchaseId = response[0].id;
+			finalResponse.purchaseName = response[0].name;
+			finalResponse.date = response[0].date;
+			finalResponse.supplierId = response[0].supplier_id;
+			finalResponse.amount = response[0].amount;
+			finalResponse.paymentStatus = response[0].payment_status;
+			finalResponse.products = [];
+
+			response.forEach((obj) => {
+				finalResponse.products.push({
+					purchaseProductId: obj.product_purchase_id,
+					productId: obj.product_id,
+					unitPrice: obj.unit_price,
+					quantity: obj.quantity,
+				});
+			});
+		}
+
+		res.json(finalResponse);
+	} catch (error) {
+		res.json({ error });
+	}
+}
+
 async function showPurchases(req, res) {
 	res.render('purchase');
 }
@@ -46,6 +79,14 @@ async function showPurchases(req, res) {
 async function createPurchase(req, res) {
 	try {
 		res.json(await createPurchaseOrder(req.body));
+	} catch (error) {
+		res.json({ error });
+	}
+}
+
+async function updatePurchase(req, res) {
+	try {
+		res.json(await updatePurchaseOrder(req.body));
 	} catch (error) {
 		res.json({ error });
 	}
@@ -59,6 +100,14 @@ async function createProductPurchase(req, res) {
 	}
 }
 
+async function updateProductPurchase(req, res) {
+	try {
+		res.json(await updateProductInPurchaseOrder(req.body));
+	} catch (error) {
+		res.json({ error });
+	}
+}
+
 module.exports = {
 	showPurchases,
 	createPurchase,
@@ -67,4 +116,7 @@ module.exports = {
 	fetchProducts,
 	fetchWarehouses,
 	createProductPurchase,
+	fetchOrderDetails,
+	updatePurchase,
+	updateProductPurchase,
 };
--- public/js/purchase/index.js
+++ public/js/purchase/index.js
@@ -1,4 +1,5 @@
 let orderId;
+let orderDetails;
 
 const patterns = {
 	textOnly: '^[a-zA-Z\\s]+$',
@@ -32,10 +33,6 @@ const validation = {
 		},
 	},
 	form2: {
-		purchase_id: {
-			required: true,
-			pattern: patterns.numberOnly,
-		},
 		product_id: {
 			required: true,
 			pattern: patterns.numberOnly,
@@ -51,31 +48,39 @@ const validation = {
 	},
 };
 
-async function generateForm1() {
+async function generateForm1(oId = null) {
 	const paymentOptions = await generateDropDown('paymentStatus');
 
 	const supplierOptions = await generateSuppliersDropDown();
+
+	await getOrderDetails(oId);
 
 	const root = document.getElementById('root');
 
 	let content = `
 	<form>
 		<div class="form-floating mb-3">
-			<input name="name" type="text" class="form-control" id="floatingName" placeholder="name">
+			<input name="name" type="text" class="form-control" id="floatingName" placeholder="name"
+				${orderDetails?.purchaseName ? `value = "${orderDetails?.purchaseName}"` : ''}
+			>
 			<label for="floatingName">Order name</label>
 			<div class="invalid-feedback">
 				Please enter a valid purchase order name.
 			</div>
 		</div>
 		<div class="form-floating mb-3">
-			<input name="date" type="date" class="form-control" id="floatingDate" placeholder="date">
+			<input name="date" type="date" class="form-control" id="floatingDate" placeholder="date"
+				${orderDetails?.date ? `value = "${orderDetails?.date}"` : ''}
+			>
 			<label for="floatingDate">Date</label>
 			<div class="invalid-feedback">
 				Please enter a valid date.
 			</div>
 		</div>
 		<div class="form-floating mb-3">
-			<select name="supplier_id" class="form-select" aria-label="select" id="floatingSupplier">
+			<select name="supplier_id" class="form-select" aria-label="select" id="floatingSupplier"
+				${orderDetails?.supplierId ? `value = "${orderDetails?.supplierId}"` : ''}
+			>
 				${supplierOptions}
 			</select>
 			<label for="floatingSupplier">Supplier</label>
@@ -84,14 +89,18 @@ async function generateForm1() {
 			</div>
 		</div>
 		<div class="form-floating mb-3">
-			<input name="amount" type="number" class="form-control" id="floatingAmount" placeholder="amount" min="0">
+			<input name="amount" type="number" class="form-control" id="floatingAmount" placeholder="amount" min="1"
+				${orderDetails?.amount ? `value = "${orderDetails?.amount}"` : ''}
+			>
 			<label for="floatingAmount">Amount</label>
 			<div class="invalid-feedback">
 				Please select a valid amount.
 			</div>
 		</div>
 		<div class="form-floating mb-3">
-			<select name="payment_status" class="form-select" aria-label="select" id="floatingPaymentStatus">
+			<select name="payment_status" class="form-select" aria-label="select" id="floatingPaymentStatus"
+				${orderDetails?.paymentStatus ? `value = "${orderDetails?.paymentStatus}"` : ''}
+			>
 				${paymentOptions}
 			</select>
 			<label for="floatingPaymentStatus">Payment</label>
@@ -101,23 +110,101 @@ async function generateForm1() {
 		</div>
 	</form>
 	<div class="d-flex mt-5 justify-content-center">
-		<button
-		type="button"
-		class="btn btn-primary"
-		onclick="submitForm1()"
-		>
-			Next
-		</button>
+		${
+			orderDetails?.purchaseId
+				? `<button
+					type="button"
+					class="btn btn-primary me-3"
+					onclick="submitForm1(1)"
+					>
+						Update
+					</button>
+
+					<button
+					type="button"
+					class="btn btn-primary"
+					onclick="generateForm2()"
+					>
+						Next
+					</button>`
+				: `<button
+					type="button"
+					class="btn btn-primary"
+					onclick="submitForm1()"
+					>
+						Submit
+					</button>`
+		}
+		
 	</div>`;
 
 	root.innerHTML = content;
 }
 
+async function submitForm1(update = false) {
+	document.getElementsByTagName('form')[0].classList.add('was-validated');
+	const nameElement = document.getElementsByName('name')[0];
+	const dateElement = document.getElementsByName('date')[0];
+	const supplierElement = document.getElementsByName('supplier_id')[0];
+	const amountElement = document.getElementsByName('amount')[0];
+	const paymentElement = document.getElementsByName('payment_status')[0];
+
+	// Clear Errors
+	Array([
+		nameElement,
+		dateElement,
+		supplierElement,
+		amountElement,
+		paymentElement,
+	]).forEach((ele) => (ele.required = false));
+
+	const data = {
+		name: nameElement.value,
+		date: dateElement.value,
+		supplier_id: supplierElement.value,
+		amount: amountElement.value,
+		payment_status: paymentElement.value,
+	};
+
+	if (checkValidation(data, validation.form1)) {
+		try {
+			const body = new URLSearchParams();
+
+			Object.entries(data).forEach((arr) => {
+				body.append(arr[0], arr[1]);
+			});
+
+			if (update) {
+				body.append('id', orderDetails.purchaseId);
+			}
+
+			const response = await fetch('api/purchase/order', {
+				method: update ? 'PUT' : 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body,
+			});
+
+			const result = await response.json();
+
+			if (result.insertId && !orderId) {
+				orderId = result.insertId;
+			}
+
+			generateForm2();
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
+
 async function generateForm2() {
 	const productOptions = await generateProductsDropDown();
-	const warehouseOptions = await generateWarehousesDropDown();
-
 	const root = document.getElementById('root');
+
+	orderId && (await getOrderDetails(orderId));
+
 	let content = `
   	<div class="d-flex mb-5 justify-content-end">
 		<button
@@ -129,117 +216,153 @@ async function generateForm2() {
 		</button>
 	</div>
 
-	<form>
+	<form class="was-validated">
 		
 	</form>
 	<div class="d-flex mt-5 justify-content-center">
 		<button
-		type="button"
-		class="btn btn-primary"
-		onclick=""
+			type="button"
+			class="btn btn-primary"
+			onclick="document.getElementById('newForm').disabled = false;"
 		>
-		Submit
+			Finish
 		</button>
 	</div>`;
 
 	root.innerHTML = content;
 	document.getElementById('addProducts').addEventListener('click', () => {
-		generateAddProductRows(productOptions, warehouseOptions);
+		document.getElementById('addProducts').disabled = true;
+		Array.from(document.getElementsByClassName('custom-disabled')).forEach(
+			(ele) => (ele.disabled = true)
+		);
+		generateAddProductRows(productOptions);
 	});
-	await generateAddProductRows(productOptions, warehouseOptions);
+	if (orderId) {
+		orderDetails.products.forEach((obj) => {
+			generateAddProductRows(productOptions, obj.purchaseProductId, { ...obj });
+		});
+	} else {
+		generateAddProductRows(productOptions);
+	}
 }
 
-async function submitForm1() {
-	document.getElementsByTagName('form')[0].classList.add('was-validated');
-	const nameElement = document.getElementsByName('name')[0];
-	const dateElement = document.getElementsByName('date')[0];
-	const supplierElement = document.getElementsByName('supplier_id')[0];
-	const amountElement = document.getElementsByName('amount')[0];
-	const paymentElement = document.getElementsByName('payment_status')[0];
+async function getOrderDetails(id) {
+	if (id) {
+		const response = await fetch(`api/order/${id}`);
+		orderId = id;
+		orderDetails = await response.json();
+	}
+}
+
+function generateAddProductRows(
+	productOptions,
+	purchaseProductId = null,
+	productDetails = null
+) {
+	document.querySelector('form').innerHTML += `
+  	<div class="container">
+		<div class="row align-items-center mb-3">
+			<div class="col">
+				<div class="form-floating">
+					<select class="form-select custom-disabled" aria-label="select" id="floatingProducts" name="product_id" required
+						${productDetails?.productId ? `value = "${productDetails?.productId}"` : ''}
+					>
+						${productOptions}
+					</select>
+					<label for="floatingProducts">Product</label>
+					<div class="invalid-feedback">
+						Please select a valid product.
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-floating">
+					<input type="number" class="form-control custom-disabled" id="floatingUnitPrice" placeholder="unitPrice" min="1" name="unit_price" required
+						${productDetails?.unitPrice ? `value = "${productDetails?.unitPrice}"` : ''}
+					>
+					<label for="floatingUnitPrice">Unit Price</label>
+					<div class="invalid-feedback">
+						Please enter a valid price.
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="form-floating">
+					<input type="number" class="form-control custom-disabled" id="floatingQuantity" placeholder="quantity" min="1" name="quantity" required
+						${productDetails?.quantity ? `value = "${productDetails?.quantity}"` : ''}
+					>
+					<label for="floatingQuantity">Quantity</label>
+					<div class="invalid-feedback">
+						Please enter a valid quantity.
+					</div>
+				</div>
+			</div>
+			<div class="w-auto">
+				<button type="button" class="btn btn-success custom-disabled" onclick="saveProduct(event,${purchaseProductId})">Save</button>
+			</div>
+			<div class="w-auto">
+				<button type="button" class="btn btn-danger custom-disabled" onclick="deleteProduct(event)">Delete</button>
+			</div>
+		</div>
+	</div>`;
+}
+
+async function saveProduct(e, purchaseProductId = null) {
+	const productElement =
+		e.target.parentElement.parentElement.children[0].children[0].children[0];
+
+	const unitPriceElement =
+		e.target.parentElement.parentElement.children[1].children[0].children[0];
+
+	const quantityElement =
+		e.target.parentElement.parentElement.children[2].children[0].children[0];
 
 	// Clear Errors
-	[
-		nameElement,
-		dateElement,
-		supplierElement,
-		amountElement,
-		paymentElement,
-	].forEach((ele) => (ele.required = false));
+	Array([productElement, unitPriceElement, quantityElement]).forEach(
+		(ele) => (ele.required = false)
+	);
 
 	const data = {
-		name: nameElement.value,
-		date: dateElement.value,
-		supplier_id: supplierElement.value,
-		amount: amountElement.value,
-		payment_status: paymentElement.value,
+		product_id: productElement.value,
+		unit_price: unitPriceElement.value,
+		quantity: quantityElement.value,
 	};
-	console.log(data);
-	if (checkValidation(data, validation.form1)) {
+
+	if (checkValidation(data, validation.form2, true)) {
 		try {
+			data.purchase_id = orderId;
+
+			if (purchaseProductId) {
+				data.id = purchaseProductId;
+			}
+
 			const body = new URLSearchParams();
 
 			Object.entries(data).forEach((arr) => {
 				body.append(arr[0], arr[1]);
 			});
 
-			const response = await fetch('api/purchase/order', {
-				method: 'POST',
+			const response = await fetch('api/purchase/product', {
+				method: purchaseProductId ? 'PUT' : 'POST',
 				headers: {
 					'Content-Type': 'application/x-www-form-urlencoded',
 				},
 				body,
 			});
+			const result = await response.json();
 
-			orderId = await response.json().insertId;
-
-			generateForm2();
+			if (!result.error) {
+				generateForm2();
+			}
 		} catch (error) {
 			console.log(error);
 		}
 	}
 }
 
-function generateAddProductRows(productOptions, warehouseOptions) {
-	document.querySelector('form').innerHTML += `
-  	<div class="container">
-		<div class="row align-items-center mb-3">
-			<div class="col">
-				<div class="form-floating">
-				<select class="form-select" aria-label="select" id="floatingProducts">
-					${productOptions}
-				</select>
-				<label for="floatingProducts">Product</label>
-			</div>
-			</div>
-			<div class="col">
-				<div class="form-floating">
-				<select class="form-select" aria-label="select" id="floatingWarehouse">
-					${warehouseOptions}
-				</select>
-				<label for="floatingWarehouse">Warehouse</label>
-				</div>
-			</div>
-			<div class="col">
-				<div class="form-floating">
-				<input type="number" class="form-control" id="floatingUnitPrice" placeholder="unitPrice" min="0">
-				<label for="floatingUnitPrice">Unit Price</label>
-				</div>
-			</div>
-			<div class="col">
-				<div class="form-floating">
-				<input type="number" class="form-control" id="floatingQuantity" placeholder="quantity" min="0">
-					<label for="floatingQuantity">Quantity</label>
-				</div>
-			</div>
-			<div class="w-auto">
-				<button type="button" class="btn btn-danger" onclick="deleteProduct(event)">Delete</button>
-			</div>
-		</div>
-	</div>`;
-}
-
 function deleteProduct(e) {
 	e.target.parentElement.parentElement.parentElement.remove();
+	generateForm2();
 }
 
 function generateDropDown(value) {
@@ -294,7 +417,7 @@ function generateWarehousesDropDown() {
 		.catch(() => '');
 }
 
-function checkValidation(body, validation) {
+function checkValidation(body, validation, special = false) {
 	const result = [];
 	for (let arr of Object.entries(validation)) {
 		const field = arr[0];
@@ -320,11 +443,17 @@ function checkValidation(body, validation) {
 			}
 		}
 	}
+
+	if (special) {
+		return result;
+	}
+
 	if (result.length > 0) {
 		result.forEach((obj) => {
 			document.getElementsByName(obj.field)[0].required = true;
 		});
 		return false;
 	}
+
 	return true;
 }
