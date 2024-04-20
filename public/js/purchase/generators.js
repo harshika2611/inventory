6b138283970d95488be54f3de45a8b91fdc194ed
aryamankarde2024 <aryaman.karde.2024@gmail.com>
2024-04-20 15:04:33 +0530

    Broken down single script in purchase module.

---
+++ public/js/purchase/generators.js
@@ -0,0 +1,60 @@
+function generateDropDown(value, selectedId) {
+  return fetch(`api/combos/${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      let content = '';
+      data.forEach((o) => {
+        content += `<option value="${o.opt_id}" ${
+          o.opt_id == selectedId ? 'selected="selected"' : ''
+        }>
+            ${o.value}
+          </option>`;
+      });
+      return content;
+    })
+    .catch(() => '');
+}
+
+function generateSuppliersDropDown() {
+  return fetch('api/purchase/suppliers')
+    .then((res) => res.json())
+    .then((data) => {
+      let content = '';
+      data.forEach((o) => {
+        content += `<option value="${o.id}">${o?.firstname.concat(
+          ' ',
+          o?.lastname
+        )}</option>`;
+      });
+      return content;
+    })
+    .catch(() => '');
+}
+
+function generateProductsDropDown(id, selectedId) {
+  return fetch('api/purchase/products/' + id)
+    .then((res) => res.json())
+    .then((data) => {
+      let content = '';
+      data.forEach((o) => {
+        content += `<option value="${o.id}" ${
+          o.id == selectedId ? 'selected="selected"' : ''
+        } >${o.product_name}</option>`;
+      });
+      return content;
+    })
+    .catch(() => '');
+}
+
+function generateWarehousesDropDown() {
+  return fetch('api/purchase/warehouses')
+    .then((res) => res.json())
+    .then((data) => {
+      let content = '';
+      data.forEach((o) => {
+        content += `<option value="${o.id}">${o.name}</option>`;
+      });
+      return content;
+    })
+    .catch(() => '');
+}
