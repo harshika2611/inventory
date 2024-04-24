b70296c5a3387a986664c692c5f9de1dcd51f60f
Dhyey Patel <dhyey.patel@esparkbizmail.com>
2024-04-24 12:20:22 +0000

    changes in productInfo module add popup and some validation

--- controller/product/productListing.js
+++ controller/product/productListing.js
@@ -2,41 +2,10 @@ const {
   getProduct,
   checkProductSevice,
   insertProductService,
-  updateProduct,
   insertProductDetailService,
 } = require('../../service/products/product');
 const logger = require('../../logs');
 
-const patterns = {
-  textOnly: '^[a-zA-Z\\s]+$',
-  numberOnly: '^\\d+$',
-};
-
-const productValid = {
-  productname: {
-    required: true,
-  },
-  skuid: {
-    required: true,
-    pattern: patterns.numberOnly,
-  },
-  category: {
-    required: true,
-    pattern: patterns.numberOnly,
-  },
-  cost: {
-    required: true,
-    pattern: patterns.numberOnly,
-  },
-  stock: {
-    required: true,
-    pattern: patterns.numberOnly,
-  },
-  description: {
-    required: false,
-    pattern: patterns.textOnly,
-  },
-};
 const productListing = (req, res) => {
   res.render('product/product', { data: req.user });
 };
@@ -52,7 +21,11 @@ const manageProduct = async (req, res) => {
       try {
         const result = await insertProductService(req.body);
         console.log(result, 'id');
-        const result1 = await insertProductDetailService(result, req.body,req.user);
+        const result1 = await insertProductDetailService(
+          result,
+          req.body,
+          req.user
+        );
         return res.status(200).send('product successfully added');
       } catch (error) {
         logger.logError(error);
@@ -65,17 +38,6 @@ const manageProduct = async (req, res) => {
   }
 };
 
-const productInfo = async (req, res) => {
-  try {
-    if (req.query.id) {
-      res.render('product/productInfo', { data: req.user });
-    } else {
-      res.render('components/errorPage');
-    }
-  } catch (err) {
-    res.render('components/errorPage');
-  }
-};
 const getApiproduct = async (req, res) => {
   try {
     let id = req.query.id || '';
@@ -89,53 +51,9 @@ const getApiproduct = async (req, res) => {
     logger.logError(err);
   }
 };
-const productInfoPost = async (req, res) => {
-  try {
-    let storage = req.user.storageId || '1';
-    await updateProduct({ ...req.body, id: req.query.id }, storage);
-    res.status(200).json('done...');
-  } catch (err) {
-    logger.logError(err);
-  }
-};
 
-function productInfoValid(valid) {
-  return ({ body }, res, next) => {
-    for (let arr of Object.entries(valid)) {
-      const field = arr[0];
-      const obj = arr[1];
-      if (obj.required) {
-        if (!body[field]) {
-          res.status(400).json({
-            status: 'error',
-            message: `${field} is required!`,
-            field,
-          });
-          return false;
-        }
-      }
-
-      // Note pattern is optional property
-      if (obj?.pattern && body[field]) {
-        if (!body[field].match(obj?.pattern)) {
-          res.status(404).json({
-            status: 'error',
-            message: `Invalid input for ${field}!`,
-            field,
-          });
-          return false;
-        }
-      }
-    }
-    next();
-  };
-}
 module.exports = {
   productListing,
-  productInfo,
   getApiproduct,
-  productInfoPost,
-  productInfoValid,
   manageProduct,
-  productValid,
 };
