e52ed041be365858a5512b796f7f2fbaac125513
aryamankarde2024 <aryaman.karde.2024@gmail.com>
2024-04-25 09:47:41 +0530

    DB Modifications.

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ const connection = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   multipleStatements: true,
-  dateStrings: true
 });
 
 connection.getConnection((err) => {
